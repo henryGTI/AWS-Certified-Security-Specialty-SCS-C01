91a5bc49d8bee23b0fa7da740e5d778b5a21754d
hyy <ssuzi97@gmail.com>
2025-05-16 08:03:44 +0900

    Update cia_example.py

--- chapter01_security_fundamentals/cia_example.py
+++ chapter01_security_fundamentals/cia_example.py
@@ -7,3 +7,9 @@ s3.upload_file(
     'uploaded_file.txt',        # S3에 저장될 이름
     ExtraArgs={'ServerSideEncryption': 'AES256'}  # 서버측 암호화
 )
+
+코드 분석
+
+ExtraArgs={'ServerSideEncryption': 'AES256'}: S3에 저장되는 파일을 자동으로 암호화(기밀성 보장)
+
+EC2에서 실행 시, IAM 역할이 S3 접근 권한을 가지고 있어야 함
