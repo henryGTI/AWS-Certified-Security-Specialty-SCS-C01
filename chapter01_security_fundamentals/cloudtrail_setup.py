e7a6b17f1d81a5b1ec71f9ed45d57eaf316423ec
hyy <ssuzi97@gmail.com>
2025-05-16 08:10:21 +0900

    Update cloudtrail_setup.py

---
+++ chapter01_security_fundamentals/cloudtrail_setup.py
@@ -0,0 +1,12 @@
+import boto3
+
+cloudtrail = boto3.client('cloudtrail')
+cloudtrail.create_trail(
+    Name='my-trail',
+    S3BucketName='my-cloudtrail-logs'
+)
+cloudtrail.start_logging(Name='my-trail')
+
+// 코드 분석
+// CloudTrail 트레일을 생성하고, S3에 로그를 저장
+// 이후 모든 AWS API 호출이 기록됨
