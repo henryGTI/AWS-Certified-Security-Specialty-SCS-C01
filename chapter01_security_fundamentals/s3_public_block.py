d09020c472a895f2e6d7b8799aaa299171f79be4
hyy <ssuzi97@gmail.com>
2025-05-16 08:08:58 +0900

    Update s3_public_block.py

---
+++ chapter01_security_fundamentals/s3_public_block.py
@@ -0,0 +1,16 @@
+import boto3
+
+s3 = boto3.client('s3')
+s3.put_public_access_block(
+    Bucket='my-bucket',
+    PublicAccessBlockConfiguration={
+        'BlockPublicAcls': True,
+        'IgnorePublicAcls': True,
+        'BlockPublicPolicy': True,
+        'RestrictPublicBuckets': True
+    }
+)
+
+// 코드 분석
+// S3 버킷의 퍼블릭 접근을 완전히 차단
+// 실수로 공개되는 것을 방지하여 데이터 유출 위험 감소
